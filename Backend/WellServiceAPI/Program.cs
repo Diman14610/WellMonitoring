44ed975ff0c8623ee6e2deb69a5be1efb09183b5
Infinity <n.diman1998@gmail.com>
2023-11-07 21:23:11 +0300

    Включил Swagger по умолчанию

--- Backend/WellServiceAPI/Program.cs
+++ Backend/WellServiceAPI/Program.cs
@@ -88,11 +88,8 @@ namespace WellServiceAPI
 
             var app = builder.Build();
 
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
+            app.UseSwagger();
+            app.UseSwaggerUI();
 
             app.UseCors(con =>
             {
